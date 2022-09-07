6ce7cf9bda44ca211a6e126e8eff867e4fe8e857
moonitil <moonitil@github.com>
2022-09-06 21:31:10 -0400

    Created basic onclick function

---
+++ rainbowClick.js
@@ -0,0 +1,8 @@
+var rainbow = document.getElementById("rainbowColors");
+
+var colors = ["red", "orange", "yellow", "green", "skyblue", "blue", "violet"];
+var number = 0;
+
+function loadRainbow(currentColor){
+    console.log(currentColor);
+}
