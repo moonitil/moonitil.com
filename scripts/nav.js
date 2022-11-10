c05093283a35c7b62d1185e9fb85eedfdb60c9a8
moonitil <moonitil@github.com>
2022-11-10 15:25:47 -0500

    Working nav bar and site updates.

--- scripts/nav.js
+++ scripts/nav.js
@@ -16,25 +16,25 @@ function navAnimation(direction1, direction2) {
 }
 
 function toggleNav() {
-    overlay.classList.toggle('overlay-active');
+    // Toggle: Fade Menu Bars
+    menuBars.classList.toggle('change');
     // Toggle: Menu Active
+    overlay.classList.toggle('overlay-active');
     if (overlay.classList.contains('overlay-active')) {
         // Animate In - Overlay 
-        overlay.classList.replace('overlay-slide-right', 'overlay-slide-left')
+        overlay.classList.replace('overlay-slide-left', 'overlay-slide-right')
         // Animate In - Nav Items
         navAnimation('out', 'in');
     } else {
         // Animate Out - Overlay
-        overlay.classList.replace('overlay-slide-left', 'overlay-slide-right')
+        overlay.classList.replace('overlay-slide-right', 'overlay-slide-left')
         // Animate Out - Nav Items
         navAnimation('in', 'out');
     }
 }
 
 // Event Listeners
-menuBars.addEventListener('click', () => {
-    toggleNav();
-});
+menuBars.addEventListener('click', toggleNav);
 navItems.forEach((nav) => {
     nav.addEventListener('click', toggleNav);
 });
